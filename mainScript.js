e6f4e0bf05b6200517ebd879a7b174c6d03c9af7
William <willev300@gmail.com>
2024-10-29 14:21:58 -0400

    Attempted work on auto sizing; need to implement whole box

--- mainScript.js
+++ mainScript.js
@@ -11,4 +11,46 @@ document.addEventListener("DOMContentLoaded", function(){
 
         mainTable.appendChild(newRow)
     }
+
+    window.addEventListener("resize", resizeElements)
 });
+
+
+
+function resizeElements(e){
+    resizeCalendarTable();
+}
+
+function resizeCalendarTable(){
+    let calendar = document.getElementById("calendar");
+    let mainTable = document.getElementById("mainTable");
+    let siblings = document.querySelectorAll("#calendar > *")
+
+    let calendarTotalHeight = getAbsoluteHeightFromElement(calendar);
+    let totalSiblingHeight = 0;
+
+    for (sibling of siblings){
+        if (sibling.id == "mainTable"){
+            console.log("Skipping " + sibling);
+            continue;
+        }
+        totalSiblingHeight += getAbsoluteHeightFromElement(sibling);
+    }
+    console.log("Total Calendar Height: " + calendarTotalHeight);
+    console.log("Total Sibling height: " + totalSiblingHeight);
+    let calendarRemainingSize = calendarTotalHeight - totalSiblingHeight;
+
+    mainTable.setAttribute("height", calendarRemainingSize + "px");
+}
+
+function getAbsoluteHeightFromElement(element){
+    let elementStyle = getComputedStyle(element);
+
+    let elementHeightFromStyle = parseInt(elementStyle.height);
+    let elementPaddingFromStyle = parseInt(elementStyle.paddingTop) + parseInt(elementStyle.paddingBottom);
+    let elementMarginFromStyle = parseInt(elementStyle.marginTop) + parse;
+
+    let elementAbsoluteHeight = elementHeightFromStyle + elementPaddingFromStyle;
+    
+    return elementAbsoluteHeight;
+}
