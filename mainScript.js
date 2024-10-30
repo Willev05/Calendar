dc9b5f34ee7ebacfadee2fc8016fec7b98e4a954
William <willev300@gmail.com>
2024-10-29 21:02:20 -0400

    Calendar now scales proprely; Need to fix side pannel and main section scale

--- mainScript.js
+++ mainScript.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let mainTable = document.getElementById("mainTable");
+    let mainTable = document.getElementById("mainTable").firstElementChild;
 
+    //Creates the basic semantic table
     for (let i = 0; i < 5; i++){
         let newRow = document.createElement("tr");
 
@@ -13,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     window.addEventListener("resize", resizeElements)
+    resizeElements();
 });
 
 
@@ -22,35 +24,42 @@ function resizeElements(e){
 }
 
 function resizeCalendarTable(){
+    //Get all required elements
     let calendar = document.getElementById("calendar");
     let mainTable = document.getElementById("mainTable");
     let siblings = document.querySelectorAll("#calendar > *")
 
-    let calendarTotalHeight = getAbsoluteHeightFromElement(calendar);
+    //Set some heights for later use
+    let calendarTotalHeight = getAbsoluteHeightFromElement(calendar); 
     let totalSiblingHeight = 0;
 
     for (sibling of siblings){
+        //Exclude table as is going to be resized later
         if (sibling.id == "mainTable"){
-            console.log("Skipping " + sibling);
             continue;
         }
+
+        
         totalSiblingHeight += getAbsoluteHeightFromElement(sibling);
     }
-    console.log("Total Calendar Height: " + calendarTotalHeight);
-    console.log("Total Sibling height: " + totalSiblingHeight);
+
     let calendarRemainingSize = calendarTotalHeight - totalSiblingHeight;
 
-    mainTable.setAttribute("height", calendarRemainingSize + "px");
+    //We only want part of it as to not stick to corners of screen
+    let calendarWantedSize = calendarRemainingSize * 0.95;
+
+    mainTable.setAttribute("height", calendarWantedSize + "px");
 }
 
+//Gets absolute height by adding margin. Returns a float !! border-box model is already in use !!
 function getAbsoluteHeightFromElement(element){
     let elementStyle = getComputedStyle(element);
 
-    let elementHeightFromStyle = parseInt(elementStyle.height);
-    let elementPaddingFromStyle = parseInt(elementStyle.paddingTop) + parseInt(elementStyle.paddingBottom);
-    let elementMarginFromStyle = parseInt(elementStyle.marginTop) + parse;
+    //Gets both the border-box values and then margin
+    let elementHeightFromStyle = parseFloat(elementStyle.height);
+    let elementMarginFromStyle = parseFloat(elementStyle.marginTop) + parseFloat(elementStyle.marginBottom);
 
-    let elementAbsoluteHeight = elementHeightFromStyle + elementPaddingFromStyle;
+    let elementAbsoluteHeight = elementHeightFromStyle + elementMarginFromStyle;
     
     return elementAbsoluteHeight;
 }
